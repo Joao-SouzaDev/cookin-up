3ff4e9191cd3cb1d7cc5a323ea31be4037e7ada6
João Souza <joao.souza@asv.com.br>
2025-03-15 15:42:25 -0300

    Adicionado item selecionavel

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Tag from './TagTexto.vue';
+export default {
+    components: {
+        Tag
+    },
+    props: {
+        ingrediente: { type: String, required: true }
+    },
+    data() {
+        return { selecionado: false }
+    }
+
+}
+</script>
+
+<template>
+    <button class="ingredientes" v-on:click="selecionado = !selecionado" :aria-pressed="selecionado">
+        <Tag :texto="ingrediente" :ativa="selecionado"/>
+    </button>
+</template>
+
+<style scoped>
+.ingredientes{
+    cursor: pointer;
+}
+</style>
